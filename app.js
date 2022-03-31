8f20bdc60a7167a8d7847e01eef08741c07c1fb6
sshyun <sshyun@amazon.com>
2022-03-31 15:40:59 +0900

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res
-) => res.send('SA Launch!'));
+) => res.send('Hello World! AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
