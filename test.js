bcc5f8ff1a7e46c91b11bb5fc69372c2f37f0af3
sshyun <sshyun@amazon.com>
2022-03-31 12:09:08 +0900

    test error fixed

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 var test = require('unit.js');
 var str = 'Hello, world!';
 
-test.string(str).startsWith('Hello1');
+test.string(str).startsWith('Hello');
 
-if (test.string(str).startsWith('Hello1')) {
+if (test.string(str).startsWith('Hello')) {
     console.log('Passed');
 }
