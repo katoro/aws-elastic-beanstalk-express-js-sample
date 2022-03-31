f7a8b1a5a39314a78b5e447de855d06e07d84883
sshyun <sshyun@amazon.com>
2022-03-31 17:01:15 +0900

    이것은 새로운 커밋입니다!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res
-) => res.send('Hello World! AWS!'));
+) => res.send('Hello World! AWSㅁㅈ워ㅏㅁ주아ㅓㅁㅈ!미ㅓㅏㅇ마저우ㅏ머주아ㅓ'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
