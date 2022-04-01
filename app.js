3325cca642997a5677172302b09281eda1d519a1
sshyun <sshyun@amazon.com>
2022-04-01 10:31:46 +0900

    안녕하세요? Any Company의 커밋/푸시 테스트를 진행해볼게요!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res
-) => res.send('<H1>Hello world, Any Company Homepage</H1>'));
+) => res.send('<H1>Hello world</H1>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
